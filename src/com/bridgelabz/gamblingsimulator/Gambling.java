c25bd04247f765c07ce10b96f7bf610344254df4
erakshaykumar <akaakshay304@gmail.com>
2022-02-09 12:16:45 +0530

    Added: UC1 Gambling Simulator logic

---
+++ src/com/bridgelabz/gamblingsimulator/Gambling.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.gamblingsimulator;
+
+public class Gambling {
+	public static void main(String[] args) {
+		System.out.println("Welcome To Gambling Simulator");	
+		int stake = 100 ;
+		int bet = 1;
+		
+	}
+
+}
