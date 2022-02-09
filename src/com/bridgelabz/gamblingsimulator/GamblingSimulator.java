8931d8b46662ba39549af9701cbb080e3a6c6786
erakshaykumar <akaakshay304@gmail.com>
2022-02-09 14:25:35 +0530

    Modified: Change Welcome message to main

--- src/com/bridgelabz/gamblingsimulator/GamblingSimulator.java
+++ src/com/bridgelabz/gamblingsimulator/GamblingSimulator.java
@@ -1,11 +1,10 @@
 package com.bridgelabz.gamblingsimulator;
 
 public class GamblingSimulator {
-	static int stake = 100 ;		 // Total Stake Value = $100
-
+	static int stake = 100 ;	// Total Stake Value = $100
+	static int bet = 1;
 	public static void checkWinner() {
-		System.out.println("Welcome To Gambling Simulator");
-		
+			
 		int TRIAL = (int) Math.floor(Math.random() * 2);
 		System.out.println("Value Is " +TRIAL);
 		if (TRIAL == 0) {   
@@ -18,6 +17,7 @@ public class GamblingSimulator {
 		System.out.println("Gambler having stake after bet are of $ " + stake);
 	}
 	public static void main(String[] args) {
+		System.out.println("Welcome To Gambling Simulator");
 		checkWinner();
 	}
 }
