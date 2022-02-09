b43e7704462d68c18dcb5332dacceacef3515734
erakshaykumar <akaakshay304@gmail.com>
2022-02-09 12:46:55 +0530

    Added: Check Winner Or Looser

--- src/com/bridgelabz/gamblingsimulator/Gambling.java
+++ src/com/bridgelabz/gamblingsimulator/Gambling.java
@@ -1,11 +1,24 @@
 package com.bridgelabz.gamblingsimulator;
 
 public class Gambling {
-	public static void main(String[] args) {
-		System.out.println("Welcome To Gambling Simulator");	
-		int stake = 100 ;
-		int bet = 1;
-		
-	}
+	static int stake = 100 ;		 // Total Stake Value = $100
 
+	public static void checkWinner() {
+		System.out.println("Welcome To Gambling Simulator");
+
+		System.out.println("Gambler start with stake of $" + stake);
+		int TRIAL = (int) Math.floor(Math.random() * 2);
+		System.out.println("Value Is " +TRIAL);
+		if (TRIAL == 0) {   
+			System.out.println("Gambler won the game");
+			stake++; 	//Increment
+		} else {
+			System.out.println("Gambler loose the game");
+			stake--;	//Decrement
+		}
+		System.out.println("Gambler having stake after bet are of $ " + stake);
+	}
+	public static void main(String[] args) {
+		checkWinner();
+	}
 }
